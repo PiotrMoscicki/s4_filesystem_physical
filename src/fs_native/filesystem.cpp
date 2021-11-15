d8ae0a9636adeb07307c15b3b843dced3469f4dd
Piotr Mościcki <m.piotr@moscicki.pl>
2021-11-15 19:21:55 +0000

    Begin implementation of the filesystem

--- src/fs_native/filesystem.cpp
+++ src/fs_native/filesystem.cpp
@@ -4,6 +4,20 @@ using namespace fs;
 using namespace fs::native;
 
 //*************************************************************************************************
-Path Filesystem::root() const {
+Res<void, ErrMount> Filesystem::mount(std::filesystem::path mount_point, std::string name) {
+    // const size_t idx = std::distance(m_names.begin()
+    //     , m_names.insert(std::upper_bound( m_names.begin(), m_names.end(), name ), std::move(name)));
+    // m_mount_points.insert(idx, std::move(mount_point));
+
+    return Ok();
+}
+
+//*************************************************************************************************
+Res<void, ErrUnmount> Filesystem::unmount(const std::string& name) {
     return {};
+}
+
+//*************************************************************************************************
+Path Filesystem::root() const {
+    return Path("/");
 }
--- src/fs_native/filesystem.hpp
+++ src/fs_native/filesystem.hpp
@@ -1,14 +1,29 @@
 #pragma once
 
+#include <vector>
+#include <filesystem>
+
 #include "ifilesystem.hpp"
 
 namespace fs::native {
+
+    enum class ErrMount {
+        ALREADY_MOUNTED,
+        INVALID_NAME,
+        MOUNT_POINT_DOESNT_EXIST
+    };
+
+    enum class ErrUnmount {
+        UNDEFINED
+    };
 
     //*********************************************************************************************
     //*********************************************************************************************
     //*********************************************************************************************
     class Filesystem : public IFilesystem {
     public:
+        Res<void, ErrMount> mount(std::filesystem::path mount_point, std::string name);
+        Res<void, ErrUnmount> unmount(const std::string& name);
 
         Path root() const override;
         Res<Path, ErrParent> parent(const Path& path) const override;
@@ -25,6 +40,10 @@ namespace fs::native {
 
         Res<rtti::Buffer, ErrRead> read(const Path& path) const override;
         Res<void, ErrWrite> write(const Path& path, const rtti::Buffer& buf) const override;
+
+    private:
+        std::vector<std::string> m_names; 
+        std::vector<std::filesystem::path> m_mount_points;
 
     }; // class IFilesystem
 
